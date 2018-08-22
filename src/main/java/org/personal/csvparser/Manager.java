5bf8116e6897d618210e53f619346de6ccd606ab
MattOxberry <tuc19266@temple.edu>
2018-08-21 21:08:53 -0400

    added manager, started to build JSON

---
+++ src/main/java/org/personal/csvparser/Manager.java
@@ -0,0 +1,14 @@
+package org.personal.csvparser;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Manager {
+
+	public JSONArray createJson(String csvString) {
+		JSONObject jo = new JSONObject();
+		csvString.split(",");
+		
+		return new JSONArray();
+	}
+}
