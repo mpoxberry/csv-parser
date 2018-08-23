1f3352d7c0a3a3097ea623bc561d439d5b21e136
mpoxberry <matthew.oxberry@qvc.com>
2018-08-23 08:04:37 -0400

    createList() junit

--- src/test/java/org/personal/csvparser/ManagerTest.java
+++ src/test/java/org/personal/csvparser/ManagerTest.java
@@ -1,14 +1,23 @@
 package org.personal.csvparser;
 
+import java.util.List;
+
 import org.junit.Test;
 
 public class ManagerTest {
-
-	
+	Manager manager = new Manager();
 	
 	@Test
 	public void createListTest() {
-		;
+		String goodInput = "Asset being sold,Asset being purchased,Dollar Value,Account Owner\r\n" + 
+				"Cash,GOOG,215,vk49No9e\r\n" + 
+				"TWTR,BTC,455,79kRyE6h\r\n" + 
+				"Cash,FB,6,vk49No9e";
+		
+		List<String> testOutput = manager.createList(goodInput);
+		
+		System.out.println(testOutput);
+		System.out.println("createTest() size: " + testOutput.size());
 	}
 
 }
