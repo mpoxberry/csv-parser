1ec5585dc5940f9bdc726b79a5d24e0e8879b2cf
MattOxberry <tuc19266@temple.edu>
2018-08-23 22:03:56 -0400

    swapped out hard coded test data

--- src/test/java/org/personal/csvparser/ManagerTest.java
+++ src/test/java/org/personal/csvparser/ManagerTest.java
@@ -4,17 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ManagerTest {
 	Manager manager = new Manager();
 	
+	final String goodCSVInput = "date,gasStation,costPerGallon,totalPurchased,milage,manualMpg,tripA,totalGas\r\n" +
+			"8/13/1998,Merit,$0.889,$10.50,240,,11.900\r\n" +
+			"8/29/1998,Texaco,$0.999,$11.00,540,27.3,--,11.010\r\n" +
+			"9/9/1998,Texaco,$0.999,$10.50,825,27.1,--,10.500";
+	
+	List<String> goodListInput = new ArrayList<>();
+	
+	@Before
+	public void setup() {
+		goodListInput.add("date,gasStation,costPerGallon,totalPurchased,milage,manualMpg,tripA,totalGas");
+		goodListInput.add("8/13/1998,Merit,$0.889,$10.50,240,,11.900");
+		goodListInput.add("8/29/1998,Texaco,$0.999,$11.00,540,27.3,--,11.010");
+		goodListInput.add("9/9/1998,Texaco,$0.999,$10.50,825,27.1,--,10.500");
+	}
+	
 	@Test
 	public void createListTest() {
-		String goodInput = "Asset being sold,Asset being purchased,Dollar Value,Account Owner\r\n" + 
-				"Cash,GOOG,215,vk49No9e\r\n" + 
-				"TWTR,BTC,455,79kRyE6h\r\n" + 
-				"Cash,FB,6,vk49No9e";
+		String goodInput = this.goodCSVInput;
+		
+		List<String> goodOutput = new ArrayList<>();
+		goodOutput.add("date,gasStation,costPerGallon,totalPurchased,milage,manualMpg,tripA,totalGas");
+		goodOutput.add("8/13/1998,Merit,$0.889,$10.50,,11.900");
+		goodOutput.add("8/29/1998,Texaco,$0.999,$11.00,540,27.3,--,11.010");
+		goodOutput.add("9/9/1998,Texaco,$0.999,$10.50,825,27.1,--,10.500");
 		
 		List<String> testOutput = manager.createList(goodInput);
 		
@@ -24,11 +43,7 @@ public class ManagerTest {
 	
 	@Test
 	public void createJsonTest() {
-		List<String> goodInput = new ArrayList<>();
-		goodInput.add("date,gasStation,costPerGallon,totalPurchased,mileage,manualMPG,tripA,totalGas");
-		goodInput.add("8/13/1998,Merit,$0.889,$10.50,240,,--,11.900");
-		goodInput.add("8/29/1998,Texaco,$0.999,$11.00,540,27.3,--,11.010");
-		goodInput.add("9/9/1998,Texaco,$0.999,$10.50,825,27.1,--,10.500");
+		List<String> goodInput = this.goodListInput;
 		
 		List<JSONObject> testOutput = manager.createJson(goodInput);
 		
