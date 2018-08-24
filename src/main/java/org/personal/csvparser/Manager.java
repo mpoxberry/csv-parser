17a4f44fdf8d1dd52ce48ddf91ad621b19df8d4f
MattOxberry <tuc19266@temple.edu>
2018-08-23 20:50:53 -0400

    formed json objects

--- src/main/java/org/personal/csvparser/Manager.java
+++ src/main/java/org/personal/csvparser/Manager.java
@@ -1,19 +1,38 @@
 package org.personal.csvparser;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Manager {
 
-	public JSONArray createJson(List<String> csvList) {
-		//TODO: make json object
-		//csvList.get(0)
-		return new JSONArray();
+	public List<JSONObject> createJson(List<String> csvList) {
+		List<String> l = csvList;
+		List<String> p = csvJsonProperties(csvList.get(0));
+		l.remove(0);
+
+		List<JSONObject> jl = new ArrayList<>();
+
+		for (String csv : l) {
+			List<String> cl = Arrays.asList(csv.split(","));
+			JSONObject jo = new JSONObject();
+
+			for (int i = 0; i < cl.size(); i++) {
+				jo.put(p.get(i), cl.get(i));
+			}
+			jl.add(jo);
+		}
+
+		return jl;
 	}
-	
-	public List<String> createList(String csvString) {	
+
+	public List<String> createList(String csvString) {
 		return Arrays.asList(csvString.split("\\r?\\n"));
+	}
+
+	public List<String> csvJsonProperties(String jsonProperties) {
+		return Arrays.asList(jsonProperties.split(","));
 	}
 }
--- src/test/java/org/personal/csvparser/ManagerTest.java
+++ src/test/java/org/personal/csvparser/ManagerTest.java
@@ -1,7 +1,9 @@
 package org.personal.csvparser;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONObject;
 import org.junit.Test;
 
 public class ManagerTest {
@@ -18,6 +20,19 @@ public class ManagerTest {
 		
 		System.out.println(testOutput);
 		System.out.println("createTest() size: " + testOutput.size());
+	}
+	
+	@Test
+	public void createJsonTest() {
+		List<String> goodInput = new ArrayList<>();
+		goodInput.add("date,gasStation,costPerGallon,totalPurchased,mileage,manualMPG,tripA,totalGas");
+		goodInput.add("8/13/1998,Merit,$0.889,$10.50,240,,--,11.900");
+		goodInput.add("8/29/1998,Texaco,$0.999,$11.00,540,27.3,--,11.010");
+		goodInput.add("9/9/1998,Texaco,$0.999,$10.50,825,27.1,--,10.500");
+		
+		List<JSONObject> testOutput = manager.createJson(goodInput);
+		
+		System.out.println(testOutput);
 	}
 
 }
